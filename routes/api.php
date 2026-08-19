1825fc04cd7f44e2a275f83754a039349a80ffcb
Rehan-Ramadhan <rehanramadhan06092008@gmail.com>
2026-08-19 14:01:57 +0700

    refactor: sesuaikan struktur relasi film dan aktor

--- routes/api.php
+++ routes/api.php
@@ -15,21 +15,21 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // Route genre
+    // route genre
     Route::get('/genre', [GenreController::class, 'index']);
     Route::post('/genre', [GenreController::class, 'store']);
     Route::get('/genre/{id}', [GenreController::class, 'show']);
     Route::put('/genre/{id}', [GenreController::class, 'update']);
     Route::delete('/genre/{id}', [GenreController::class, 'destroy']);
 
-    // Route aktor
+    // route aktor
     Route::get('/aktor', [AktorController::class, 'index']);
     Route::post('/aktor', [AktorController::class, 'store']);
     Route::get('/aktor/{id}', [AktorController::class, 'show']);
     Route::put('/aktor/{id}', [AktorController::class, 'update']);
     Route::delete('/aktor/{id}', [AktorController::class, 'destroy']);
 
-    // Route film
+    // route film
     Route::get('/film', [FilmController::class, 'index']);
     Route::post('/film', [FilmController::class, 'store']);
     Route::get('/film/{id}', [FilmController::class, 'show']);
